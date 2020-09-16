41435a66083008c13899b744dc36acd7ad46629d
EX <1721179627@qq.com>
2020-09-16 16:03:40 +0800

    update for ...

--- package/upapps/luci-theme-edge/htdocs/luci-static/edge/js/script.js
+++ package/upapps/luci-theme-edge/htdocs/luci-static/edge/js/script.js
@@ -1,289 +1,224 @@
 /**
- *  Argon is a clean HTML5 theme for LuCI. It is based on luci-theme-material and Argon Template
+ *  Edge is a clean HTML5 theme for LuCI. It is based on luci-theme-Argon
  *
- *  luci-theme-argon
- *      Copyright 2019 Jerrykuku <jerrykuku@qq.com>
+ *  luci-theme-edge
+ *      Copyright 2020 Garypang <garyp@qq.com>
  *
  *  Have a bug? Please create an issue here on GitHub!
- *      https://github.com/jerrykuku/luci-theme-argon/issues
+ *      https://github.com/garypang13/luci-theme-edge/issues
  *
- *  luci-theme-bootstrap:
- *      Copyright 2008 Steven Barth <steven@midlink.org>
- *      Copyright 2008 Jo-Philipp Wich <jow@openwrt.org>
- *      Copyright 2012 David Menting <david@nut-bolt.nl>
- *
- *  MUI:
- *      https://github.com/muicss/mui
- *
- *  luci-theme-material:
- *      https://github.com/LuttyYang/luci-theme-material/
+ *  luci-theme-material: 
+ *      Copyright 2015 Lutty Yang <lutty@wcan.in>
+ *		https://github.com/LuttyYang/luci-theme-material/
  *
  *  Agron Theme
  *	    https://demos.creative-tim.com/argon-dashboard/index.html
  *
  *  Login background
  *      https://unsplash.com/
+ *  Font generate by Icomoon<icomoon.io>
  *
  *  Licensed to the public under the Apache License 2.0
  */
 
-/*
- *  Font generate by Icomoon<icomoon.io>
- */
+document.addEventListener('luci-loaded', function(ev) {
 (function ($) {
-    $(".main > .loading").fadeOut();
+	$(".main > .loading").fadeOut();
 
-    /**
-     * trim text, Remove spaces, wrap
-     * @param text
-     * @returns {string}
-     */
-    function trimText(text) {
-        return text.replace(/[ \t\n\r]+/g, " ");
-    }
+	/**
+	 * trim text, Remove spaces, wrap
+	 * @param text
+	 * @returns {string}
+	 */
+	function trimText(text) {
+		return text.replace(/[ \t\n\r]+/g, " ");
+	}
 
+	var lastNode = undefined;
+	var mainNodeName = undefined;
 
-    var lastNode = undefined;
-    var mainNodeName = undefined;
+	var nodeUrl = "";
+	(function(node){
+		var luciLocation;
+		if (node[0] == "admin"){
+			luciLocation = [node[1], node[2]];
+		}else{
+			luciLocation = node;
+		}
 
-    var nodeUrl = "";
-    (function (node) {
-        if (node[0] == "admin") {
-            luciLocation = [node[1], node[2]];
-        } else {
-            luciLocation = node;
-        }
+		for(var i in luciLocation){
+			nodeUrl += luciLocation[i];
+			if (i != luciLocation.length - 1){
+				nodeUrl += "/";
+			}
+		}
+	})(luciLocation);
 
-        for (var i in luciLocation) {
-            nodeUrl += luciLocation[i];
-            if (i != luciLocation.length - 1) {
-                nodeUrl += "/";
-            }
-        }
-    })(luciLocation);
+	/**
+	 * get the current node by Burl (primary)
+	 * @returns {boolean} success?
+	 */
+	function getCurrentNodeByUrl() {
+		var ret = false;
+		if (!$('body').hasClass('logged-in')) {
+			luciLocation = ["Main", "Login"];
+			return true;
+		}
 
-    /**
-     * get the current node by Burl (primary)
-     * @returns {boolean} success?
-     */
-    function getCurrentNodeByUrl() {
-        var ret = false;
-        if (!$('body').hasClass('logged-in')) {
-            luciLocation = ["Main", "Login"];
-            return true;
-        }
-        $(".main > .main-left > .nav > .slide > .active").next(".slide-menu").stop(true).slideUp("fast");
-        $(".main > .main-left > .nav > .slide > .menu").removeClass("active");
-        $(".main > .main-left > .nav > .slide > .menu").each(function () {
-            var ulNode = $(this);
+		$(".main > .main-left > .nav > .slide > .menu").each(function () {
+			var ulNode = $(this);
+			ulNode.next().find("a").each(function () {
+				var that = $(this);
+				var href = that.attr("href");
 
-            ulNode.next().find("a").each(function () {
-                var that = $(this);
-                var href = that.attr("href");
+				if (href.indexOf(nodeUrl) != -1) {
+					ulNode.click();
+					ulNode.next(".slide-menu").stop(true, true);
+					lastNode = that.parent();
+					lastNode.addClass("active");
+					ret = true;
+					return true;
+				}
+			});
+		});
+		return ret;
+	}
 
-                if (href.indexOf(nodeUrl) != -1) {
-                    ulNode.click();
-                    ulNode.next(".slide-menu").stop(true, true);
-                    lastNode = that.parent();
-                    lastNode.addClass("active");
-                    ret = true;
-                    return true;
-                }
-            });
-        });
-        return ret;
-    }
+	/**
+	 * menu click
+	 */
+	$(".main > .main-left > .nav > .slide > .menu").click(function () {
+		var ul = $(this).next(".slide-menu");
+		var menu = $(this);
+		if (!ul.is(":visible")) {
+			menu.addClass("active");
+			ul.addClass("active");
+			ul.stop(true).slideDown("fast");
+		} else {
+			ul.stop(true).slideUp("fast", function () {
+				menu.removeClass("active");
+				ul.removeClass("active");
+			});
+		}
+		return false;
+	});
 
-    /**
-     * menu click
-     */
-    $(".main > .main-left > .nav > .slide > .menu").click(function () {
-        var ul = $(this).next(".slide-menu");
-        var menu = $(this);
-        if (!menu.hasClass("exit")) {
-            $(".main > .main-left > .nav > .slide > .active").next(".slide-menu").stop(true).slideUp("fast");
-            $(".main > .main-left > .nav > .slide > .menu").removeClass("active");
-            if (!ul.is(":visible")) {
-                menu.addClass("active");
-                ul.addClass("active");
-                ul.stop(true).slideDown("fast");
-            } else {
-                ul.stop(true).slideUp("fast", function () {
-                    menu.removeClass("active");
-                    ul.removeClass("active");
-                });
-            }
+	/**
+	 * hook menu click and add the hash
+	 */
+	$(".main > .main-left > .nav > .slide > .slide-menu > li > a").click(function () {
+		if (lastNode != undefined) lastNode.removeClass("active");
+		$(this).parent().addClass("active");
+		$(".main > .loading").fadeIn("fast");
+		return true;
+	});
 
-            return false;
-        }
+	/**
+	 * fix menu click
+	 */
+	$(".main > .main-left > .nav > .slide > .slide-menu > li").click(function () {
+		if (lastNode != undefined) lastNode.removeClass("active");
+		$(this).addClass("active");
+		$(".main > .loading").fadeIn("fast");
+		window.location = $($(this).find("a")[0]).attr("href");
+		return false;
+	});
 
-    });
+	/**
+	 * get current node and open it
+	 */
+	if (getCurrentNodeByUrl()) {
+		mainNodeName = "node-" + luciLocation[0] + "-" + luciLocation[1];
+		mainNodeName = mainNodeName.replace(/[ \t\n\r\/]+/g, "_").toLowerCase();
+		$("body").addClass(mainNodeName);
+	}
 
+	/**
+	 * Sidebar expand
+	 */
+	var showSide = false;
+	$(".showSide").click(function () {
+		if (showSide) {
+			$(".darkMask").stop(true).fadeOut("fast");
+			$(".main-left").stop(true).animate({
+				width: "0"
+			}, "200");
+			$(".main-right").css("overflow-y", "visible");
+			showSide = false;
+		} else {
+			$(".darkMask").stop(true).fadeIn("fast");
+			$(".main-left").stop(true).animate({
+				width: "13rem"
+			}, "200");
+			$(".main-right").css("overflow-y", "hidden");
+			showSide = true;
+		}
+	});
 
+	$(".darkMask").click(function () {
+		if (showSide) {
+			showSide = false;
+			$(".darkMask").stop(true).fadeOut("fast");
+			$(".main-left").stop(true).animate({
+				width: "0"
+			}, "fast");
+			$(".main-right").css("overflow-y", "visible");
+		}
+	});
 
+	$(window).resize(function () {
+		if ($(window).width() > 921) {
+			$(".main-left").css("width", "");
+			$(".darkMask").stop(true);
+			$(".darkMask").css("display", "none");
+			showSide = false;
+		}
+	});
 
-// define what element should be observed by the observer
-// and what types of mutations trigger the callback
-    if ($("#cbi-dhcp-lan-ignore").length > 0) {
-        observer.observe(document.getElementById("cbi-dhcp-lan-ignore"), {
-            subtree: true,
-            attributes: true
-        });
-    }
+	/**
+	 * fix legend position
+	 */
+	$("legend").each(function () {
+		var that = $(this);
+		that.after("<span class='panel-title'>" + that.text() + "</span>");
+	});
 
-    /**
-     * hook menu click and add the hash
-     */
-    $(".main > .main-left > .nav > .slide > .slide-menu > li > a").click(function () {
-        if (lastNode != undefined)
-            lastNode.removeClass("active");
-        $(this).parent().addClass("active");
-        $(".main > .loading").fadeIn("fast");
-        return true;
-    });
+	$(".cbi-section-table-titles, .cbi-section-table-descr, .cbi-section-descr").each(function () {
+		var that = $(this);
+		if (that.text().trim() == ""){
+			that.css("display", "none");
+		}
+	});
 
-    /**
-     * fix menu click
-     */
-    $(".main > .main-left > .nav > .slide > .slide-menu > li").click(function () {
-        if (lastNode != undefined)
-            lastNode.removeClass("active");
-        $(this).addClass("active");
-        $(".main > .loading").fadeIn("fast");
-        window.location = $($(this).find("a")[0]).attr("href");
-        return false;
-    });
-    
-    /**
-     * fix submenu click
-     */
-    $("#maincontent > .container > .tabs > li").click(function () {
-        $(".main > .loading").fadeIn("fast");
-        window.location = $($(this).find("a")[0]).attr("href");
-        return false;
-    });
+	$(".main-right").focus();
+	$(".main-right").blur();
+	$("input").attr("size", "0");
+	$(".cbi-button-up").val("__");
+	$(".cbi-button-down").val("__");
+	$(".slide > a").removeAttr("href");
 
-    /**
-     * get current node and open it
-     */
-    if (getCurrentNodeByUrl()) {
-        mainNodeName = "node-" + luciLocation[0] + "-" + luciLocation[1];
-        mainNodeName = mainNodeName.replace(/[ \t\n\r\/]+/g, "_").toLowerCase();
-        $("body").addClass(mainNodeName);
-    }
-    $(".cbi-button-up").val("");
-    $(".cbi-button-down").val("");
-
-
-    /**
-     * hook other "A Label" and add hash to it.
-     */
-    $("#maincontent > .container").find("a").each(function () {
-        var that = $(this);
-        var onclick = that.attr("onclick");
-        if (onclick == undefined || onclick == "") {
-            that.click(function () {
-                var href = that.attr("href");
-                if (href.indexOf("#") == -1) {
-                    $(".main > .loading").fadeIn("fast");
-                    return true;
-                }
-            });
-        }
-    });
-
-    /**
-     * Sidebar expand
-     */
-    var showSide = false;
-    $(".showSide").click(function () {
-        if (showSide) {
-            $(".darkMask").stop(true).fadeOut("fast");
-            $(".main-left").stop(true).animate({
-                width: "0"
-            }, "fast");
-            $(".main-right").css("overflow-y", "auto");
-            showSide = false;
-        } else {
-            $(".darkMask").stop(true).fadeIn("fast");
-            $(".main-left").stop(true).animate({
-                width: "15rem"
-            }, "fast");
-            $(".main-right").css("overflow-y", "hidden");
-            showSide = true;
-        }
-    });
-
-
-    $(".darkMask").click(function () {
-        if (showSide) {
-            showSide = false;
-            $(".darkMask").stop(true).fadeOut("fast");
-            $(".main-left").stop(true).animate({
-                width: "0"
-            }, "fast");
-            $(".main-right").css("overflow-y", "auto");
-        }
-    });
-
-    $(window).resize(function () {
-        if ($(window).width() > 921) {
-            $(".main-left").css("width", "");
-            $(".darkMask").stop(true);
-            $(".darkMask").css("display", "none");
-            showSide = false;
-        }
-    });
-
-    /**
-     * fix legend position
-     */
-    $("legend").each(function () {
-        var that = $(this);
-        that.after("<span class='panel-title'>" + that.text() + "</span>");
-    });
-
-    $(".cbi-section-table-titles, .cbi-section-table-descr, .cbi-section-descr").each(function () {
-        var that = $(this);
-        if (that.text().trim() == "") {
-            that.css("display", "none");
-        }
-    });
-
-    $(".node-main-login > .main .cbi-value.cbi-value-last .cbi-input-text").focus(function () {
-        //$(".node-main-login > .main > .main-right > .login-bg").addClass("blur");
-    });
-    $(".node-main-login > .main .cbi-value.cbi-value-last .cbi-input-text").blur(function () {
-        //$(".node-main-login > .main > .main-right > .login-bg").removeClass("blur");
-    });
-
-
-    $(".main-right").focus();
-    $(".main-right").blur();
-    $("input").attr("size", "0");
-
-    if (mainNodeName != undefined) {
-        console.log(mainNodeName);
-        switch (mainNodeName) {
-            case "node-status-system_log":
-            case "node-status-kernel_log":
-                $("#syslog").focus(function () {
-                    $("#syslog").blur();
-                    $(".main-right").focus();
-                    $(".main-right").blur();
-                });
-                break;
-            case "node-status-firewall":
-                var button = $(".node-status-firewall > .main fieldset li > a");
-                button.addClass("cbi-button cbi-button-reset a-to-btn");
-                break;
-            case "node-system-reboot":
-                var button = $(".node-system-reboot > .main > .main-right p > a");
-                button.addClass("cbi-button cbi-input-reset a-to-btn");
-                break;
-        }
-    }
-
+	if (mainNodeName != undefined) {
+		console.log(mainNodeName);
+		switch (mainNodeName) {
+			case "node-status-system_log":
+			case "node-status-kernel_log":
+				$("#syslog").focus(function () {
+					$("#syslog").blur();
+					$(".main-right").focus();
+					$(".main-right").blur();
+				});
+				break;
+			case "node-status-firewall":
+				var button = $(".node-status-firewall > .main fieldset li > a");
+				button.addClass("cbi-button cbi-button-reset a-to-btn");
+				break;
+			case "node-system-reboot":
+				var button = $(".node-system-reboot > .main > .main-right p > a");
+				button.addClass("cbi-button cbi-input-reset a-to-btn");
+				break;
+		}
+	}
+	
    var getaudio = $('#player')[0];
    /* Get the audio from the player (using the player's ID), the [0] is necessary */
    var mouseovertimer;
@@ -344,23 +279,15 @@
      audiostatus = 'off';
      /*Set the status back to off*/
    });
-     setTimeout(function(){
-   if (!$('#player')[0].paused) {
-         $('.speaker').addClass('speakerplay');
-         getaudio.load();
-         getaudio.play();
-         audiostatus = 'on';
-       }
-}, 300); 
 	setTimeout(function(){
 var config = {
     // How long Waves effect duration 
     // when it's clicked (in milliseconds)
     duration: 600
 };
-    Waves.attach(".cbi-button,.btn,button,input[type='button'],input[type='reset'],input[type='submit']", ['waves-light']);
+    Waves.attach("button,input[type='button'],input[type='reset'],input[type='submit']", ['waves-light']);
 	// Ripple on hover
-$(".cbi-button,.btn,button,input[type='button'],input[type='reset'],input[type='submit']").mouseenter(function() {
+$("button,input[type='button'],input[type='reset'],input[type='submit']").mouseenter(function() {
     Waves.ripple(this, {wait: null});
 }).mouseleave(function() {
     Waves.calm(this);
@@ -368,13 +295,15 @@ $(".cbi-button,.btn,button,input[type='button'],input[type='reset'],input[type='
   Waves.init(config);
 $(".waves-input-wrapper").filter(function () {
   if($(this).children().css("display")=="none"){
-        return true;
-    }else{
-        return false;
-    }
+        return true;
+    }else{
+        return false;
+    }
 }).hide();
 
-	$("div>select:first-child,div>input[type='text']:first-child,div>input[type='email']:first-child,div>input[type='url']:first-child,div>input[type='date']:first-child,div>input[type='datetime']:first-child,div>input[type='tel']:first-child,div>input[type='number']:first-child,div>input[type='search']:first-child").after("<span class='focus-input'></span>");
+$("select,input[type='text'],input[type='email'],input[type='url'],input[type='date'],input[type='datetime'],input[type='tel'],input[type='number'],input[type='search']").filter(function () {
+return (!$(this).parents(".cbi-dynlist").length)
+}).after("<span class='focus-input'></span>");
 
 $("input[type='checkbox']").filter(function () {
   return (!$(this).next("label").length)
@@ -388,4 +317,25 @@ $("select,input").filter(function () {
   $(this).css("border-bottom","1px solid #9e9e9e");
 });
 	}, 400);
+
+var options = { attributes: true};
+function callback() {
+$("select,input[type='text'],input[type='email'],input[type='url'],input[type='date'],input[type='datetime'],input[type='tel'],input[type='number'],input[type='search']").filter(function () {
+return (!$(this).parents(".cbi-dynlist").length)
+}).after("<span class='focus-input'></span>");
+$("select,input").filter(function () {
+  return ($(this).next(".focus-input").length)
+}).focus(function(){
+  $(this).css("border-bottom","1px solid #fff");
+}).blur(function(){
+  $(this).css("border-bottom","1px solid #9e9e9e");
+});
+$("input[type='checkbox']").filter(function () {
+  return (!$(this).next("label").length)
+}).css({"position":"relative","opacity":"1","pointer-events":"auto"});
+}
+var mutationObserver = new MutationObserver(callback);
+ mutationObserver.observe($("body")[0], options);
+ $(".cbi-value").has("textarea").css("background","none");
 })(jQuery);
+});
